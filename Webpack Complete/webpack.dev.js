b85cd3bd8577413bbea69e464bc44446f2f9c96a
dhruw <dhruwlalan22@gmail.com>
2020-08-20 15:39:34 +0530

    Extracted CSS

--- Webpack Complete/webpack.dev.js
+++ Webpack Complete/webpack.dev.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
 	mode: 'development',
@@ -14,11 +15,12 @@ module.exports = {
 	module: {
 		rules: [
 			{ test: /\.js$/, exclude: /node_modules/, use: ["babel-loader"] },
-			{ test: /\.scss$/ , use: ["style-loader", "css-loader", "sass-loader"] }
+			{ test: /\.scss$/ , use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"] }
 		]
 	},
 	plugins: [
 		new HtmlWebpackPlugin({ template: path.resolve(__dirname, "src", "index.html") }),
+		new MiniCssExtractPlugin({ filename: 'style.css' }),
 		new CleanWebpackPlugin(),
 	]
 };
