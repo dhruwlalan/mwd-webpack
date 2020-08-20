f26317318114821291097bb70c9f6a6615c1c3ef
dhruw <dhruwlalan22@gmail.com>
2020-08-20 16:44:13 +0530

    Implemented html-loader & file-loader

--- Webpack Complete/webpack.dev.js
+++ Webpack Complete/webpack.dev.js
@@ -4,22 +4,43 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-	mode: 'development',
+	mode: 'development' ,
 	entry: {
 		index: './src/js/index.js'
-	},
+	} ,
 	output: {
-		filename: 'bundle.js',
-		path: path.resolve(__dirname, "dist")
-	},
+		filename: 'bundle.js' ,
+		path: path.resolve(__dirname, 'dist')
+	} ,
 	module: {
 		rules: [
-			{ test: /\.js$/, exclude: /node_modules/, use: ["babel-loader"] },
-			{ test: /\.scss$/ , use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"] }
+			{ test: /\.js$/, exclude: /node_modules/, use: ['babel-loader'] } ,
+			{ test: /\.scss$/ , use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'] } ,
+			{ test: /\.html$/ , use: ['html-loader'] } , 
+			{ 
+				test: /\.(jpeg|png|jpg|gif)$/ , 
+				use: {
+					loader: 'file-loader' , 
+					options: { 
+						name: '[name].[ext]' , 
+						outputPath: 'assets/images'
+					}
+				}
+			},
+			{ 
+				test: /\.svg$/ , 
+				use: {
+					loader: 'file-loader' , 
+					options: { 
+						name: '[name].[ext]' , 
+						outputPath: 'assets/svg'
+					}
+				}
+			},
 		]
 	},
 	plugins: [
-		new HtmlWebpackPlugin({ template: path.resolve(__dirname, "src", "index.html") }),
+		new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'src', 'index.html') }),
 		new MiniCssExtractPlugin({ filename: 'style.css' }),
 		new CleanWebpackPlugin(),
 	]
--- Webpack Complete/webpack.prod.js
+++ Webpack Complete/webpack.prod.js
@@ -4,23 +4,44 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-	mode: 'production',
+	mode: 'production' ,
 	entry: {
 		index: './src/js/index.js'
-	},
+	} ,
 	output: {
-		filename: 'bundle.[contentHash].js',
-		path: path.resolve(__dirname, "dist")
-	},
+		filename: 'bundle.[contentHash].js' ,
+		path: path.resolve(__dirname, 'dist')
+	} ,
 	module: {
 		rules: [
-			{ test: /\.js$/, exclude: /node_modules/, use: ["babel-loader"] },
-			{ test: /\.scss$/ , use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"] }
+			{ test: /\.js$/, exclude: /node_modules/, use: ['babel-loader'] } ,
+			{ test: /\.scss$/ , use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'] } ,
+			{ test: /\.html$/ , use: ['html-loader'] } , 
+			{ 
+				test: /\.(jpeg|png|jpg|gif)$/ , 
+				use: {
+					loader: 'file-loader' , 
+					options: { 
+						name: '[name].[ext]' , 
+						outputPath: 'assets/images'
+					}
+				}
+			} ,
+			{ 
+				test: /\.svg$/ , 
+				use: {
+					loader: 'file-loader' , 
+					options: { 
+						name: '[name].[ext]' , 
+						outputPath: 'assets/svg'
+					}
+				}
+			} ,
 		]
-	},
+	} ,
 	plugins: [
-		new HtmlWebpackPlugin({ template: path.resolve(__dirname, "src", "index.html") }),
-		new MiniCssExtractPlugin({ filename: 'style.[contentHash].css' }),
+		new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'src', 'index.html') }) ,
+		new MiniCssExtractPlugin({ filename: 'style.[contentHash].css' }) ,
 		new CleanWebpackPlugin()
 	]
 };
