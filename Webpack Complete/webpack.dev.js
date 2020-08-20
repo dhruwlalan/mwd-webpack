8e4e55cdaf44db5f560eb3c56cebd894d6ff69b6
dhruw <dhruwlalan22@gmail.com>
2020-08-20 13:02:30 +0530

    Installed favicon-webpack-plugin

--- Webpack Complete/webpack.dev.js
+++ Webpack Complete/webpack.dev.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 
 module.exports = {
 	mode: 'development',
@@ -18,5 +19,6 @@ module.exports = {
 	},
 	plugins: [
 		new HtmlWebpackPlugin({ template: path.resolve(__dirname, "src", "index.html") }),
+		new FaviconsWebpackPlugin({ logo: './src/assets/images/favicon.ico', prefix: 'assets/images' }),
 	]
 };
