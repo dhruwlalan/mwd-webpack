a923c5eaf5b36343be03c6f9117c1f3303711445
dhruw <dhruwlalan22@gmail.com>
2020-09-04 11:03:01 +0530

    Webpack-VueJS: npm start script

--- Webpack-VueJS/webpack.prod.js
+++ Webpack-VueJS/webpack.prod.js
@@ -12,58 +12,84 @@ module.exports = {
 	entry: { index: './src/js/index.js' } ,
 	output: {
 		filename: '[name].[contentHash].bundle.js' ,
-		path: path.resolve(__dirname, 'dist')
+		path: path.resolve(__dirname, 'dist') ,
 	} ,
 	module: {
 		rules: [
-			{ test: /\.vue$/, loader: 'vue-loader' } ,
-			{ test: /\.js$/, exclude: /node_modules/, use: ['babel-loader'] } ,
-			{ test: /\.scss$/ , use: [
-				MiniCssExtractPlugin.loader, 
-				{
-					loader: 'css-loader',
-					options: {
-			          url: false,
-			        },
-				}, 
-				'sass-loader']
+			{
+				test: /\.vue$/ ,
+				loader: 'vue-loader' ,
 			} ,
-			{ test: /\.html$/ , use: ['html-loader'] } , 
-			{ test: /\.(jpeg|png|jpg|gif)$/ , use: {loader: 'file-loader' , options: {name: '[name].[ext]' , esModule: false , outputPath: 'assets/images'}} },
-			{ test: /\.svg$/ , use: {loader: 'file-loader' , options: {name: '[name].[ext]' , outputPath: 'assets/svg'}} },
-			{ test: /\.ico$/ , use: {loader: 'file-loader' , options: {name: 'favicon.ico' , outputPath: 'assets/favicon'}} },
+			{
+				test: /\.js$/ ,
+				exclude: /node_modules/ ,
+				use: ['babel-loader'] ,
+			} ,
+			{
+				test: /\.scss$/ ,
+				use: [
+					MiniCssExtractPlugin.loader ,
+					{ loader: 'css-loader', options: { url: false } } ,
+					'sass-loader' ,
+				] ,
+			} ,
+			{
+				test: /\.html$/ ,
+				use: ['html-loader'] ,
+			} ,
+			{
+				test: /\.(jpeg|png|jpg|gif)$/ ,
+				use: {
+					loader: 'file-loader' ,
+					options: { name: '[name].[ext]' , esModule: false , outputPath: 'assets/images' } ,
+				} ,
+			} ,
+			{
+				test: /\.svg$/ ,
+				use: {
+					loader: 'file-loader' ,
+					options: { name: '[name].[ext]' , outputPath: 'assets/svg' } ,
+				} ,
+			} ,
+			{
+				test: /\.ico$/ ,
+				use: {
+					loader: 'file-loader' ,
+					options: { name: 'favicon.ico' , outputPath: 'assets/favicon' } ,
+				} ,
+			} ,
 		]
 	} ,
 	plugins: [
-		new HtmlWebpackPlugin({ 
-			filename: 'index.html',
-			template: path.resolve(__dirname, 'src', 'index.html'),
-			chunks: ['index']
-		}),
+		new HtmlWebpackPlugin({
+			filename: 'index.html' ,
+			template: path.resolve(__dirname, 'src', 'index.html') ,
+			chunks: ['index'] ,
+		}) ,
 		new MiniCssExtractPlugin({ filename: 'style.[contentHash].css' }) ,
 		new CleanWebpackPlugin() ,
 		new VueLoaderPlugin() ,
 	] ,
 	optimization: {
-		minimizer: [ new OptimizeCssAssetsPlugin() , new TerserPlugin() ],
+		minimizer: [ new OptimizeCssAssetsPlugin() , new TerserPlugin() ] ,
 		splitChunks: {
             cacheGroups: {
                 vendors: {
-                    test: /[\\/]node_modules[\\/]/,
-                    name: 'vendor',
-                    chunks: 'all',
-                    enforce: true
+                    test: /[\\/]node_modules[\\/]/ ,
+                    name: 'vendor' ,
+                    chunks: 'all' ,
+                    enforce: true ,
                 }
             }
         }
 	} ,
     resolve: {
 		alias: {
-		  'vue$': 'vue/dist/vue.esm.js'
-		},
-		extensions: ['*', '.js', '.vue', '.json']
-	},
+		  'vue$': 'vue/dist/vue.esm.js' ,
+		} ,
+		extensions: ['*', '.js', '.vue', '.json'] ,
+	} ,
 	performance: {
-		hints: 'warning'
-	},
+		hints: 'warning' ,
+	} ,
 };
--- Webpack-VueJS/webpack.start.js
+++ Webpack-VueJS/webpack.start.js
@@ -7,61 +7,87 @@ module.exports = {
 	devtool: '#eval-source-map' ,
 	entry: { index: './src/js/index.js' } ,
 	output: {
-		filename: '[name].bundle.js' ,	
+		filename: '[name].bundle.js' ,
 		path: path.resolve(__dirname, 'dist') ,
 	} ,
 	module: {
 		rules: [
-			{ test: /\.vue$/, loader: 'vue-loader' } ,
-			{ test: /\.js$/, exclude: /node_modules/, use: ['babel-loader'] } ,
-			{ test: /\.scss$/ , use: [
-				'style-loader', 
-				{
-					loader: 'css-loader',
-					options: {
-			          url: false,
-			        },
-				}, 
-				'sass-loader']
+			{
+				test: /\.vue$/ ,
+				loader: 'vue-loader' ,
 			} ,
-			{ test: /\.html$/ , use: ['html-loader'] } ,
-			{ test: /\.(jpeg|png|jpg|gif)$/ , use: {loader: 'file-loader' , options: {name: '[name].[ext]' , esModule: false , outputPath: 'assets/images'}} },
-			{ test: /\.svg$/ , use: {loader: 'file-loader' , options: {name: '[name].[ext]' , outputPath: 'assets/svg'}} },
-			{ test: /\.ico$/ , use: {loader: 'file-loader' , options: {name: 'favicon.ico' , outputPath: 'assets/favicon'}} },
+			{
+				test: /\.js$/ ,
+				exclude: /node_modules/ ,
+				use: ['babel-loader'] ,
+			} ,
+			{
+				test: /\.scss$/ ,
+				use: [
+					'style-loader' ,
+					{ loader: 'css-loader' , options: { url: false } } ,
+					'sass-loader' ,
+				] ,
+			} ,
+			{
+				test: /\.html$/ ,
+				use: ['html-loader'] ,
+			} ,
+			{ 
+				test: /\.(jpeg|png|jpg|gif)$/ ,
+				use: {
+					loader: 'file-loader' ,
+					options: { name: '[name].[ext]' , esModule: false , outputPath: 'assets/images' } ,
+				} ,
+			} ,
+			{
+				test: /\.svg$/ ,
+				use: {
+					loader: 'file-loader' ,
+					options: { name: '[name].[ext]' , outputPath: 'assets/svg' } ,
+				} ,
+			} ,
+			{
+				test: /\.ico$/ ,
+				use: {
+					loader: 'file-loader' ,
+					options: { name: 'favicon.ico' , outputPath: 'assets/favicon' } ,
+				} ,
+			} ,
 		]
-	},
+	} ,
 	plugins: [
 		new HtmlWebpackPlugin({ 
-			filename: 'index.html',
-			template: path.resolve(__dirname, 'src', 'index.html'),
-			chunks: ['index']
+			filename: 'index.html' ,
+			template: path.resolve(__dirname, 'src', 'index.html') ,
+			chunks: ['index'] ,
 		}) ,
 		new VueLoaderPlugin() ,
-	],
+	] ,
 	optimization: {
         splitChunks: {
             cacheGroups: {
                 vendors: {
-                    test: /[\\/]node_modules[\\/]/,
-                    name: 'vendor',
-                    chunks: 'all',
-                    enforce: true
-                }
+                    test: /[\\/]node_modules[\\/]/ ,
+                    name: 'vendor' ,
+                    chunks: 'all' ,
+                    enforce: true ,
+                } ,
             }
         }
-    },
+    } ,
     resolve: {
 		alias: {
-		  'vue$': 'vue/dist/vue.esm.js'
-		},
-		extensions: ['*', '.js', '.vue', '.json']
-	},
+		  'vue$': 'vue/dist/vue.esm.js' ,
+		} ,
+		extensions: ['*', '.js', '.vue', '.json'] ,
+	} ,
 	devServer: {
-	    historyApiFallback: true,
-	    noInfo: true,
-	    overlay: true
-	},
+	    historyApiFallback: true ,
+	    noInfo: true ,
+	    overlay: true ,
+	} ,
 	performance: {
-		hints: false
-	},
+		hints: false ,
+	} ,
 };
