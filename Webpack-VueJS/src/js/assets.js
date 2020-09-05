ddb0bced5c325680d6eab8523ad307b9afe0a4b9
dhruw <dhruwlalan22@gmail.com>
2020-09-05 11:52:44 +0530

    Webpack-VueJS: Restructured Code

--- Webpack-VueJS/src/js/assets.js
+++
@@ -1,6 +0,0 @@
-// SASS
-import '../sass/main.scss';
-
-// ASSETS
-import '../assets/favicon/favicon.ico';
-// import '../assets/images/logo.png';
--- Webpack-VueJS/src/js/index.js
+++ Webpack-VueJS/src/js/index.js
@@ -1,9 +1,9 @@
 // ASSETS
-import './assets.js';
+import './assets/assets.js';
 
 // MAIN
 import Vue from 'vue';
-import App from './Vue/App.vue';
+import App from './App.vue';
 
 console.log(Vue);
 
