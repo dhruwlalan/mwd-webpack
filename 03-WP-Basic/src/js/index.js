7402cc943d675f34a7ac3fedaa2f97a2dba7ee24
dhruw <dhruwlalan22@gmail.com>
2020-12-10 17:33:34 +0530

    Add: EsLint & .editorconfig

--- 03-WP-Basic/src/js/index.js
+++ 03-WP-Basic/src/js/index.js
@@ -1,19 +1,17 @@
-import './assets.js';
-import $ from "jquery";
+import './assets';
+import $ from 'jquery';
 import moment from 'moment';
 
-const greet = (name) => {
-	return `Welcome, ${name}!`;
-}
+const greet = (name) => `Welcome, ${name}!`;
 
 document.getElementById('greet-btn').addEventListener('click', () => {
-	const input = $('#input').val();
-	const now = moment().toString();
-	console.log(now);
-	if (input) {
-		const output = greet(input);
-		document.getElementById('output').textContent = output;
-	} else {
-		document.getElementById('output').textContent = '';
-	}	
+   const input = $('#input').val();
+   const now = moment().toString();
+   console.log(now);
+   if (input) {
+      const output = greet(input);
+      document.getElementById('output').textContent = output;
+   } else {
+      document.getElementById('output').textContent = '';
+   }
 });
