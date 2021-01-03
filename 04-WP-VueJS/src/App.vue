2ed17b9f71fdd65e44f2e2e8abaaef87815ff5a4
dhruw <dhruwlalan22@gmail.com>
2021-01-03 23:18:35 +0530

    Finalized Project

--- 04-WP-VueJS/src/App.vue
+++ 04-WP-VueJS/src/App.vue
@@ -1,7 +1,23 @@
 <template>
    <div class="container">
-      <h1>Hello, World!!</h1>
-      <button @click="this.count++">count: {{ count }}</button>
+      <div class="header">
+         <img class="header__img" src="./assets/images/webpack.png" />
+         <h1 class="header__title">Webpack Demo</h1>
+      </div>
+      <div class="input-group">
+         <label class="input-group__label" for="input">Enter your name: </label>
+         <input
+            class="input-group__input"
+            type="text"
+            id="input"
+            v-model="name"
+            autocomplete="off"
+         />
+      </div>
+      <button class="btn" @click="greet">
+         Greet<img class="svg-icon" src="./assets/svg/greet.svg" />
+      </button>
+      <h4 id="output">{{ showName }}</h4>
    </div>
 </template>
 
@@ -9,8 +25,18 @@
 export default {
    data() {
       return {
-         count: 0,
+         name: '',
+         showName: '',
       };
+   },
+   methods: {
+      greet() {
+         if (this.name !== '') {
+            this.showName = `Hello, ${this.name}`;
+         } else {
+            this.showName = '';
+         }
+      },
    },
 };
 </script>
--- 04-WP-VueJS/src/assets/assets.js
+++ 04-WP-VueJS/src/assets/assets.js
@@ -1,2 +1,3 @@
 import './favicon/favicon.ico';
 import '../sass/main.scss';
+import './svg/greet.svg';
